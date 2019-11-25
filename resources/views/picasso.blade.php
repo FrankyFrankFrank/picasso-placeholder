401f2b296b74f0760e0feee1b77edc82c84846a7
Adam Frank <a.frank@vehikl.com>
2019-11-25 16:30:03 -0500

    i dunno.... adjustments

--- resources/views/picasso.blade.php
+++ resources/views/picasso.blade.php
@@ -4,20 +4,20 @@
 >
     <defs>
         <linearGradient id="linear_gradient_01" gradientTransform="rotate(90)">
-            <stop offset="5%"  stop-color="hsl({{ $background_hue + 30}}, 80%, 90%)" />
-            <stop offset="95%" stop-color="hsl({{ $background_hue + 30 }}, 80%, 70%)" />
+            <stop offset="5%"  stop-color="hsl({{ $background_hue + 30 }}, 80%, 70%)" />
+            <stop offset="95%" stop-color="hsl({{ $background_hue }}, 80%, 50%)" />
         </linearGradient>
         <filter id="shadow">
           <feDropShadow dx="1" dy="1" stdDeviation="2" flood-color="hsla({{ $background_hue + 30 }}, 80%, 20%, 0.3)" />
         </filter>
     </defs>
 
-    <rect x="0"  y="0" width="{{ $width }}" height="{{ $height }}" fill="hsl({{ $background_hue }}, 80%, 90%)" />
-    <circle cx="{{ $right_eye_x_position }}" cy="{{ $right_eye_y_position }}" r="{{ $right_eye_radius }}" fill="hsl({{ $background_hue - 90}}, 80%, 90%)" />
+    <rect x="0"  y="0" width="{{ $width }}" height="{{ $height }}" fill="hsl({{ $background_hue }}, 20%, 90%)" />
+    <circle cx="{{ $right_eye_x_position }}" cy="{{ $right_eye_y_position }}" r="{{ $right_eye_radius }}" fill="hsl({{ $background_hue}}, 80%, 70%)" />
     <polygon
-        points="0,0 {{ $left_rectangle_width }},0 {{ $left_rectangle_point_three }},{{ $height / 2 }} {{ $left_rectangle_point_four }},{{ $height / 2}} {{ $left_rectangle_width }},{{ $height }} 0,{{ $height }}"
+        points="0,0 {{ $left_rectangle_width + $nose_width / 2 }},0 {{ $left_rectangle_point_three }},{{ $nose_height }} {{ $left_rectangle_point_four }},{{ $nose_height }} {{ $left_rectangle_width - $nose_width / 2 }},{{ $height }} 0,{{ $height }}"
         style="filter:url(#shadow);"
         fill="url('#linear_gradient_01')"
     />
-    <circle cx="{{ $left_eye_x_position }}" cy="{{ $left_eye_y_position }}" r="{{ $left_eye_radius }}" fill="hsl({{ $background_hue + 90}}, 80%, 90%)" style="filter:url(#shadow);" />
+    <circle cx="{{ $left_eye_x_position }}" cy="{{ $left_eye_y_position }}" r="{{ $left_eye_radius }}" fill="hsl({{ $background_hue}}, 80%, 90%)" style="filter:url(#shadow);" />
 </svg>
--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,16 @@ Route::get('/{width}/{height}', function ($width, $height) {
     $background_hue = rand(0, 360);
 
     $left_rectangle_width = rand($width / 4, $width * 0.9);
-    $left_rectangle_point_three = $left_rectangle_width + 10;
-    $left_rectangle_point_four = $left_rectangle_width - 10;
+    $nose_height = rand($height / 4, $height / 1.2);
+    $nose_width = rand($width / 30, $width / 10);
+    $left_rectangle_point_three = $left_rectangle_width + $nose_width / 2;
+    $left_rectangle_point_four = $left_rectangle_width - $nose_width / 2;
 
-    $left_eye_radius = rand($width / 10, $width / 8);
+    $left_eye_radius = rand(min([$width, $height]) / 10, min([$width, $height]) / 8);
     $left_eye_x_position = rand(0 + $left_eye_radius, $left_rectangle_width - $left_eye_radius);
     $left_eye_y_position = rand($height / 4, $height * 0.7);
 
-    $right_eye_radius = rand($width / 10, $width / 8);
+    $right_eye_radius = rand(min([$width, $height]) / 20, min([$width, $height]) / 10);
     $right_eye_x_position = rand($left_rectangle_width + $right_eye_radius, $width - $right_eye_radius);
     $right_eye_y_position = rand($height / 4, $height * 0.7);
 
@@ -33,6 +35,10 @@ Route::get('/{width}/{height}', function ($width, $height) {
         'left_rectangle_width' => $left_rectangle_width,
         'left_rectangle_point_three' => $left_rectangle_point_three,
         'left_rectangle_point_four' => $left_rectangle_point_four,
+
+        'nose_height' => $nose_height,
+        'nose_width' => $nose_width,
+
         'left_eye_radius' => $left_eye_radius,
         'left_eye_x_position' => $left_eye_x_position,
         'left_eye_y_position' => $left_eye_y_position,
