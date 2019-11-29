b6814be8c1c30be389734ce27dabb27cf5db9cdb
Adam Frank <a.frank@vehikl.com>
2019-11-29 12:53:55 -0500

    adding documentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,8 @@
             .gallery-grid {
                 display: grid;
                 grid-template-rows: 50vh 50vh;
-                grid-template-columns: 50vw 50vw;
+                grid-template-columns: 2fr 1fr;
+                grid-gap: 1rem;
                 grid-template-areas:
                     "left upperright"
                     "left lowerright"
@@ -38,14 +39,33 @@
         </div>
         <div class="bg-gray-800 px-12 py-16 text-gray-100">
             <section class="mb-16">
-                <h2 class="font-bold">What is Picassio?</h2>
-                <p class="mb-4">Use it to generate svg placeholder images for any purpose.</p>
+                <h2 class="font-bold text-lg mb-4">What is Picassio?</h2>
+                <p class="mb-4 text-gray-300">Use it to generate svg placeholder images for any purpose.</p>
                 <script src="https://gist.github.com/FrankyFrankFrank/342a427cbc4dedcf381f19516c4113a2.js"></script>
             </section>
             <section class="mb-16">
-                <h2 class="font-bold">Changing the size</h2>
-                <p class="mb-4">To change the size of the generated image, change the url.</p>
-                <p class="mb-4 inline-block">For example, if you want to generate a placholder image 500px wide and 200px high, use <pre class="bg-gray-900 inline p-4 m-1">picass.io/500/200</pre> as the image source.</p>
+                <h2 class="font-bold text-lg mb-4">Changing the size</h2>
+                <p class="mb-2 text-gray-300">To change the size of the generated image, change the url.</p>
+                <p class="mb-2 text-gray-300">The height and width are set by the first and second directory in the url, respectively.</p>
+                <p class="mb-2 text-gray-300">For example, if you want to generate a placholder image 500px wide and 200px high, change the url to:</p>
+                <pre class="bg-gray-900 p-2 mt-4">picass.io/500/200</pre>
+            </section>
+            <section class="mb-16 flex flex-wrap">
+                <div class="w-full md:w-1/2 mb-8">
+                    <h2 class="font-bold text-lg mb-4">Setting a base hue</h2>
+                    <p class="mb-2 text-gray-300">Picassio automatically generates a pleasing set of shapes and colours based on a random base hue.</p>
+                    <p class="mb-2 text-gray-300">If you would like to constrain the image to a particular base hue between 0 and 360, you may do so by adding a query parameter.</p>
+                    <p class="mb-2 text-gray-300">For example, if you want to generate a placholder image with a red base hue, add the following to the end of the url:</p>
+                    <pre class="bg-gray-900 p-2 mt-4 mb-2">?hue=360</pre>
+                    <p class="mb-2 text-gray-300 inline-block">Therefore a 400px wide 200px high red-based placeholder image would have this url:</p>
+                    <pre class="bg-gray-900 p-2 mt-4">picass.io/400/200?hue=360</pre>
+                </div>
+                <div class="w-full md:w-1/2 flex items-center justify-center">
+                    <div>
+                        <img src="/400/200?hue=360" class="shadow-lg" />
+                        <figcaption>picass.io/400/200?hue=360</figcaption>
+                    </div>
+                </div>
             </section>
         </div>
     </body>
