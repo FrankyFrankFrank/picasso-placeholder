c456804133590e5101b23692544b27c0e887b18b
Adam Frank <a.frank@vehikl.com>
2019-12-06 13:59:44 -0500

    Update SvgRequestTest.php

--- tests/Unit/SvgRequestTest.php
+++ tests/Unit/SvgRequestTest.php
@@ -18,4 +18,13 @@ class SvgRequestTest extends TestCase
         $this->assertTrue($request->hue() >= 0);
         $this->assertTrue($request->hue() <= 360);
     }
+
+    /** @test **/
+    public function it_returns_a_random_saturation_if_not_present_in_query()
+    {
+        $request = SvgRequest::create(route('generate', ['width' => 100, 'height' => 100]));
+
+        $this->assertTrue($request->saturation() >= 0);
+        $this->assertTrue($request->saturation() <= 100);
+    }
 }
