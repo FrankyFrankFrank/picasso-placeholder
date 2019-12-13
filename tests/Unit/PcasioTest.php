d7e79be433c14dc19e527c4b015ec0d0102bc5e4
Adam Frank <a.frank@vehikl.com>
2019-12-13 10:41:06 -0500

    left eye positioning test

--- tests/Unit/PcasioTest.php
+++ tests/Unit/PcasioTest.php
@@ -89,4 +89,46 @@ class PcasioTest extends TestCase
             )
         );
     }
+
+    /** @test **/
+    public function itHasTheCorrectLeftEyePositioning()
+    {
+        $width = '1000';
+        $height = '150';
+
+        $expected_min_radius = '10';
+        $expected_max_radius = '75';
+
+        $expected_min_x_position = '-500';
+        $expected_max_x_position = '0';
+
+        $expected_min_y_position = -$height / 4;
+        $expected_max_y_position = $height / 4;
+
+        $response = $this->get('/'. $width . '/' . $height);
+
+        $this->assertThat(
+            $response->viewData('left_eye_radius'),
+            $this->logicalAnd(
+                $this->greaterThan($expected_min_radius),
+                $this->lessThan($expected_max_radius)
+            )
+        );
+
+        $this->assertThat(
+            $response->viewData('left_eye_x_position'),
+            $this->logicalAnd(
+                $this->greaterThan($expected_min_x_position),
+                $this->lessThan($expected_max_x_position)
+            )
+        );
+
+        $this->assertThat(
+            $response->viewData('left_eye_y_position'),
+            $this->logicalAnd(
+                $this->greaterThan($expected_min_y_position),
+                $this->lessThan($expected_max_y_position)
+            )
+        );
+    }
 }
